71dc671c6795bb18bdf5239142892dbda9d98607
Mustafaaa Darwish <mustafa.darwish@y42.com>
2024-04-16 14:12:54 +0200

    test

--- src/models/my_first_model.sql
+++ src/models/my_first_model.sql
@@ -1,6 +1,6 @@
 {#
 
-🚀 Run `y42 build --select my_first_model` to materialize this model
+🚀 test Run `y42 build --select my_first_model` to materialize this model
 
 Read the docs: https://docs.y42.dev/canonical/build-command
 
